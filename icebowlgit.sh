11bdc3e1b417c02c07452c694e8195054ae4d10f
icebowl <sift@owt.com>
2019-02-04 08:47:01 -0800

    AA

---
+++ icebowlgit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  icebowl;
+git config --global user.name  icebowl;
+git config --global user.user icebowl;
+git config --global user.email sift@owt.com;
+git config --list;
