1df5a7df37037d584f95c177d97f8018150b8483
icebowl <icebowl1967@gmail.com>
2020-02-19 20:46:46 -0800

    AA

--- sed-script.sh
+++ sed-script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+sed  -i 's/\\r//g' *.txt;
+sed  -i 's/\\n/\n/g' *.txt;
 sed -i "s/b'//g" *.txt;
-sed -i "s/r//g" *.txt;
-sed -i "s/n//g" *.txt;
 sed -i "s/'//g" *.txt;
-#sed -i "s/\//R/g" *.txt;
-tr '\' 'R'  *.txt
 # https://www.cyberciti.biz/faq/how-to-use-sed-to-find-and-replace-text-in-files-in-linux-unix-shell/
