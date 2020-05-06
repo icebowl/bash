1f289dadf68a31bb3cde8bb584e522f4f0f8ad0c
zjs81 <30362347+zjs81@users.noreply.github.com>
2020-05-05 17:44:52 -0700

    Update music-loop-esc.sh
    This code makes it easier to add new songs to the playlist. The @ means it will play all alternatively you could use a * instead.

--- music/music-loop-esc.sh
+++ music/music-loop-esc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-while true
+#Creates Array Of music
+playlist=("SantanaKeysBlackMagicWoman.mp4" "thriller.mp3" "witchyWoman.mp3" "ghostbusters.mp3")
+
+#For Each entry it will play that song. c_song stands for current song
+for c_song in ${playlist[*]};
 do
-ffplay -nodisp -autoexit SantanaKeysBlackMagicWoman.mp4;
-ffplay -nodisp -autoexit thriller.mp3;
-ffplay -nodisp -autoexit witchyWoman.mp3;
-ffplay -nodisp -autoexit ghostbusters.mp3;
+ffplay -nodisp -autoexit $c_song 
 done
