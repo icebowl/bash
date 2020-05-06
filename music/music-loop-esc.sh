547513095b42e60f47447752c1a12d8347e8ae99
zjs81 <30362347+zjs81@users.noreply.github.com>
2020-05-05 18:39:08 -0700

    Update music-loop-esc.sh

--- music/music-loop-esc.sh
+++ music/music-loop-esc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #Creates Array Of music
 playlist=("SantanaKeysBlackMagicWoman.mp4" "thriller.mp3" "witchyWoman.mp3" "ghostbusters.mp3")
-
-#For Each entry it will play that song. c_song stands for current song
+#keeps the forloop looping with a while
+while true
+do
+#For Each entry it will play that song. This is a nested for loop
 for c_song in ${playlist[*]};
 do
-ffplay -nodisp -autoexit $c_song 
+ffplay -nodisp -autoexit $c_song
 done
+done 
